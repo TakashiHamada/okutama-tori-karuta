1ab9affaab38eb559365251062dd823824caebda
TakashiHamada <hamada@gift10.net>
2023-03-23 04:13:06 -0700

    日原までの山札

--- Piles.js
+++ Piles.js
@@ -2,22 +2,37 @@ function Piles() {
     let birds = new Birds();
     // --
     this.kumotoriyama = {
-        cards: [birds.hiyodori, birds.misosazai],
+        cards: [birds.misosazai, birds.uguisu, birds.kakesu, birds.komadori, birds.fukurou,
+            birds.koruri, birds.ruribitaki, birds.higara, birds.toratsugumi, birds.aogera,
+            birds.ooakagera],
         location: '雲取山',
         image: 'otherImages/kumotoriyama.jpg',
     };
     this.takanosuyama = {
-        cards: [birds.hiyodori, birds.misosazai],
+        cards: [birds.hiyodori, birds.misosazai, birds.ooruri, birds.kibitaki, birds.kogera,
+            birds.kakesu, birds.ikaru, birds.komadori, birds.yabusame, birds.fukurou,
+            birds.aobato, birds.kijibato, birds.yamadori, birds.koruri, birds.ruribitaki,
+            birds.shijyukara, birds.higara, birds.akahara, birds.toratsugumi, birds.aogera,
+            birds.ooakagera],
         location: '鷹ノ巣山',
         image: 'otherImages/takanosuyama.jpg',
     };
     this.yamanofurusatomura = {
-        cards: [birds.hiyodori, birds.misosazai],
+        cards: [birds.hojiro, birds.hiyodori, birds.misosazai, birds.uguisu, birds.ooruri,
+            birds.kibitaki, birds.kogera, birds.tobi, birds.kakesu, birds.kisekirei,
+            birds.hashibosogarasu, birds.gabicho, birds.ikaru, birds.jyobitaki, birds.yabusame,
+            birds.sankoucho, birds.fukurou, birds.aobato, birds.kijibato, birds.yamadori,
+            birds.mozu, birds.shijyukara, birds.yamasemi, birds.kawasemi, birds.higara,
+            birds.toratsugumi, birds.aogera],
         location: '山のふるさと村',
         image: 'otherImages/yamanofurusatomura.jpg',
     };
     this.nippara = {
-        cards: [birds.hiyodori, birds.misosazai, birds.oorui, birds.kibitaki, birds.kakesu, birds.kisekirei, birds.fukurou, birds.ikaru, birds.aobato, birds.toratsugumi, birds.hojiro],
+        cards: [birds.hojiro, birds.hiyodori, birds.misosazai, birds.uguisu, birds.ooruri,
+            birds.kibitaki, birds.kogera, birds.kakesu, birds.kisekirei, birds.hashibosogarasu,
+            birds.gabicho, birds.ikaru, birds.jyobitaki, birds.yabusame, birds.fukurou,
+            birds.aobato, birds.kijibato, birds.mozu, birds.shijyukara, birds.higara,
+            birds.toratsugumi, birds.aogera,],
         location: '日原',
         image: 'otherImages/nippara.jpg',
     };
@@ -47,9 +62,15 @@ function Piles() {
         image: 'otherImages/mitakesan.jpg',
     };
     this.all = {
+        cards: [birds.hojiro, birds.hiyodori, birds.misosazai, birds.uguisu, birds.ooruri,
+            birds.kibitaki, birds.kogera, birds.tobi, birds.kakesu, birds.suzume,
+            birds.kisekirei, birds.hashibosogarasu, birds.gabicho, birds.ikaru, birds.jyobitaki,
+            birds.komadori, birds.yabusame, birds.sankoucho, birds.fukurou, birds.aobato,
+            birds.kijibato, birds.yamadori, birds.koruri, birds.ruribitaki, birds.mozu,
+            birds.shijyukara, birds.yamasemi, birds.kawasemi, birds.higara, birds.akahara,
+            birds.toratsugumi, birds.aogera, birds.ooakagera],
         cards: [birds.hiyodori, birds.misosazai],
         location: '全部MIX',
-        image: 'otherImages/all.jpg',
     };
     this.getPile = function (type) {
         switch (type) {
