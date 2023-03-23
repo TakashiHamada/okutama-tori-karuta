f2b1cbc29836ca1ef08a97fefa6f03fce9cececd
TakashiHamada <hamada@gift10.net>
2023-03-23 03:43:10 -0700

    ３３種類の登録（呼び出しのみ）

--- Birds.js
+++ Birds.js
@@ -1,21 +1,61 @@
 function Birds() {
     // birds
+    // 1-
+    this.hojiro = new Bird('hojiro', 'ホオジロ');
     this.hiyodori = new Bird('hiyodori', 'ヒヨドリ');
     this.misosazai = new Bird('misosazai', 'ミソサザイ');
+    this.uguisu = new Bird('uguisu', 'ウグイス');
     this.oorui = new Bird('ooruri', 'オオルリ');
     this.kibitaki = new Bird('kibitaki', 'キビタキ');
+    this.kogera = new Bird('kogera', 'コゲラ');
+    this.tobi = new Bird('tobi', 'トビ');
     this.kakesu = new Bird('kakesu', 'カケス');
+    this.suzume = new Bird('suzume', 'スズメ');
+    // 11-
     this.kisekirei = new Bird('kisekirei', 'キセキレイ');
-    this.fukurou = new Bird('fukurou', 'フクロウ');
+    this.hashibosogarasu= new Bird('hashibosogarasu', 'ハシボソガラス');
+    this.gabicho = new Bird('gabicho', 'ガビチョウ');
     this.ikaru = new Bird('ikaru', 'イカル');
+    this.shobitaki = new Bird('shobitaki', 'ショウビタキ');
+    this.komadori = new Bird('komadori', 'コマドリ');
+    this.yabusame = new Bird('yabusame', 'ヤブサメ');
+    this.sankoucho = new Bird('sankoucho', 'サンコウチョウ');
+    this.fukurou = new Bird('fukurou', 'フクロウ');
     this.aobato = new Bird('aobato', 'アオバト');
+    // 21-
+    this.kijibato = new Bird('kijibato', 'キジバト');
+    this.yamadori = new Bird('yamadori', 'ヤマドリ');
+    this.koruri = new Bird('koruri', 'コルリ');
+    this.ruribitaki = new Bird('ruribitaki', 'ルリビタキ');
+    this.mozu = new Bird('mozu', 'モズ');
+    this.shijyukara = new Bird('shijyukara', 'シジュウカラ');
     this.yamasemi = new Bird('yamasemi', 'ヤマセミ');
     this.kawasemi = new Bird('kawasemi', 'カワセミ');
+    this.higara = new Bird('higara', 'ヒガラ');
+    this.akahara = new Bird('akahara', 'アカハラ');
+    // 31-
     this.toratsugumi = new Bird('toratsugumi', 'トラツムギ');
-    this.hojiro = new Bird('hojiro', 'ホオジロ');
+    this.aogera = new Bird('aogera', 'アオゲラ');
+    this.ooakagera = new Bird('ooakagera', 'オオアカゲラ');
 }
 
 function Bird(filePrefix, name) {
     this.filePrefix = filePrefix;
     this.name = name;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
