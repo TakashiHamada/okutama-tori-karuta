53888c1ca174f6299be3d5e0fca36540fea327af
TakashiHamada <hamada@gift10.net>
2023-06-05 03:11:23 +0900

    出現する鳥の数を更新（全体的に厳選した）

--- Piles.js
+++ Piles.js
@@ -9,9 +9,8 @@ function Piles() {
         filePrefix: 'kumotoriyama',
     };
     this.takanosuyama = {
-        cards: [birds.hiyodori, birds.misosazai, birds.ooruri, birds.kibitaki, birds.kogera,
-            birds.kakesu, birds.ikaru, birds.komadori, birds.yabusame, birds.fukurou,
-            birds.aobato, birds.kijibato, birds.yamadori, birds.koruri, birds.ruribitaki,
+        cards: [birds.ooruri, birds.kibitaki, birds.kogera, birds.kakesu, birds.komadori,
+            birds.fukurou, birds.aobato, birds.yamadori, birds.koruri, birds.ruribitaki,
             birds.shijyukara, birds.higara, birds.akahara, birds.toratsugumi, birds.aogera,
             birds.ooakagera],
         location: '鷹ノ巣山',
@@ -29,50 +28,44 @@ function Piles() {
     };
     this.nippara = {
         cards: [birds.hojiro, birds.hiyodori, birds.misosazai, birds.uguisu, birds.ooruri,
-            birds.kibitaki, birds.kogera, birds.kakesu, birds.kisekirei, birds.hashibosogarasu,
-            birds.gabicho, birds.ikaru, birds.jyobitaki, birds.yabusame, birds.fukurou,
-            birds.aobato, birds.kijibato, birds.mozu, birds.shijyukara, birds.higara,
-            birds.toratsugumi, birds.aogera,],
+            birds.kibitaki, birds.kogera, birds.kakesu, bards.suzume, birds.kisekirei,
+            birds.hashibosogarasu, birds.gabicho, birds.jyobitaki, birds.fukurou, birds.kijibato,
+            birds.shijyukara, birds.toratsugumi, birds.aogera,],
         location: '日原',
         filePrefix: 'nippara',
     };
     this.shiromaru = {
-        cards: [birds.hiyodori, birds.misosazai, birds.ooruri, birds.kibitaki, birds.kakesu,
-            birds.kisekirei, birds.fukurou, birds.ikaru, birds.aobato, birds.yamasemi,
-            birds.kawasemi],
+        cards: [birds.hiyodori, birds.misosazai, birds.uguisu, birds.ooruri, birds.tobi,
+            birds.kakesu, birds.kisekirei, birds.gabicho, birds.yabusame, birds.aobato,
+            birds.kijibato, birds.shijyukara, birds.kawasemi, birds.higara],
         location: '白丸湖',
         filePrefix: 'shiromaruko',
     };
     this.hikawa = {
-        cards: [birds.hojiro, birds.hiyodori, birds.misosazai, birds.uguisu, birds.ooruri,
-            birds.kibitaki, birds.kogera, birds.tobi, birds.suzume, birds.kisekirei,
-            birds.hashibosogarasu, birds.gabicho, birds.jyobitaki, birds.kijibato, birds.mozu,
-            birds.shijyukara, birds.yamasemi, birds.kawasemi, birds.aogera],
+        cards: [birds.hiyodori, birds.misosazai, birds.uguisu, birds.ooruri, birds.tobi,
+            birds.suzume, birds.kisekirei, birds.hashibosogarasu, birds.gabicho, birds.kijibato,
+            birds.mozu, birds.shijyukara],
         location: '氷川',
         filePrefix: 'hikawa',
     };
     this.ogouchijinja = {
-        cards: [birds.hiyodori, birds.misosazai, birds.ooruri,
-            birds.kibitaki, birds.kogera, birds.tobi, birds.kakesu, birds.kisekirei,
-            birds.fukurou, birds.kijibato, birds.shijyukara, birds.yamasemi, birds.kawasemi,
-            birds.higara, birds.aogera],
+        cards: [birds.kogera, birds.tobi, birds.kakesu, birds.fukurou, birds.kijibato,
+            birds.shijyukara, birds.yamasemi, birds.kawasemi, birds.higara, birds.aogera],
         location: '小河内神社',
         filePrefix: 'ogouchijinja',
     };
     this.hinoharatominnomori = {
         cards: [birds.misosazai, birds.uguisu, birds.ooruri, birds.kibitaki, birds.kogera,
-            birds.kakesu, birds.kisekirei, birds.ikaru, birds.komadori, birds.yabusame,
-            birds.fukurou, birds.aobato, birds.kijibato, birds.yamadori, birds.koruri,
-            birds.ruribitaki, birds.shijyukara, birds.higara, birds.akahara, birds.toratsugumi,
+            birds.kakesu, birds.kisekirei, birds.yabusame, birds.aobato, birds.yamadori,
+            birds.koruri, birds.shijyukara, birds.higara, birds.akahara, birds.toratsugumi,
             birds.aogera, birds.ooakagera],
         location: '檜原都民の森',
         filePrefix: 'hinoharatominnomori',
     };
     this.mitakesan = {
-        cards: [birds.hojiro, birds.hiyodori, birds.misosazai, birds.uguisu, birds.ooruri,
-            birds.kibitaki, birds.kogera, birds.kakesu, birds.kisekirei, birds.hashibosogarasu,
-            birds.gabicho, birds.ikaru, birds.jyobitaki, birds.yabusame, birds.fukurou,
-            birds.aobato, birds.kijibato, birds.yamadori, birds.shijyukara, birds.higara,
+        cards: [birds.misosazai, birds.uguisu, birds.ooruri, birds.kibitaki, birds.kogera,
+            birds.kakesu, birds.kisekirei, birds.gabicho, birds.ikaru, birds.yabusame,
+            birds.fukurou, birds.aobato, birds.yamadori, birds.shijyukara, birds.higara,
             birds.toratsugumi, birds.aogera,],
         location: '御岳山',
         filePrefix: 'mitakesan',
